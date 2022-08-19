1073a85642179ee7a57e234386fca1c7dc79416c
fallialfarisi15 <farismuhamad526@gmail.com>
2022-08-19 12:48:45 +0700

    Tugas 4

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, ScrollView, SafeAreaView, FlatList, Image } from 'react-native'
+import React, {useState} from 'react'
+import ImagePickerExample from './Imagepicker';
 
-export default function App() {
+
+const numColumns = 2;
+
+export default function Home () {
+    const inisialdata = [
+        {
+            id :1,
+            imageUri: 'https://selerasa.com/wp-content/uploads/2015/12/images_daging_ayam-goreng.jpg',
+        },
+        {
+            id :2, 
+            imageUri: 'https://cdn-2.tstatic.net/tribunnews/foto/bank/images/tips-pasti-jadi-bikin-rendang-pakai-rice-cooker.jpg',
+        },
+       
+       
+    ];
+
+    const [foto, setFoto] = useState(inisialdata);
+
+    const renderItem  = ({item}) => {
+        return (
+            <View style={styles.renderitem}>
+                <Image source={{ uri: item.imageUri}} style={styles.image}/>
+            </View>
+        )
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View >
+            <FlatList
+            numColumns={2}
+            showsHorizontalScrollIndicator={false}
+            data={foto}
+            keyExtractor={(item)=>item.id}
+            renderItem={renderItem}
+            />
+        </View>
+        <ImagePickerExample/>
+        
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+        flex: 1,
+        marginHorizontal: 5,
+        paddingTop: 11,
+        backgroundColor:'#fdfdfd'
   },
-});
+    body: {
+        height: 150,
+    },
+    image: {
+        flex: 1,
+        height: 180,
+        resizeMode: 'cover',
+        borderRadius: 8,
+    },
+
+    renderitem: {
+        flex: 1,
+        height: 190,
+        justifyContent: 'center',
+        marginTop: 10,
+        marginHorizontal: 5,
+        
+    }
+})
